e2651f52517e93f5252ad470c68633693badf98e
cppservergit <cppserver@martincordova.com>
2025-12-19 18:01:13 -0400

    sonarcloud issues fixed

--- src/server.hpp
+++ src/server.hpp
@@ -27,7 +27,7 @@
 #include <cstdint>
 #include <chrono> // Added for timeouts
 
-inline constexpr auto g_version = "1.1.1";
+inline constexpr auto g_version = "1.1.2";
 
 using dispatch_task = std::function<void()>;
 
