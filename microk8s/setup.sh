1166289b94b537cf5a77381d539f82bae11860c4
cppservergit <cppserver@martincordova.com>
2025-12-23 00:04:13 -0400

    sonarcloud issues fixed

--- microk8s/setup.sh
+++ microk8s/setup.sh
@@ -1,13 +1,14 @@
 VM_IP=$(ip -4 addr show scope global | grep inet | awk '{print $2}' | cut -d/ -f1 | head -n1)
 sudo apt update && sudo apt upgrade -y
+sudo apt install uuid -y
 sudo snap install microk8s --classic
 sudo microk8s status --wait-ready
 sudo microk8s enable hostpath-storage
 sudo microk8s enable ingress
 sudo microk8s enable metrics-server
 sudo microk8s enable registry
-sudo microk8s status --wait-ready
 sudo microk8s enable metallb:${VM_IP}-${VM_IP}
+sudo microk8s status --wait-ready
 
 # --- Wait for ingress controller pods to be Ready --- 
 echo "[+] Waiting for ingress controller pods..." 
