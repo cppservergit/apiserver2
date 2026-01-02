7ef4915ab34e65294c507d977825536909c959df
cppservergit <cppserver@martincordova.com>
2026-01-02 15:27:13 -0400

    sonarcloud issues fixed

--- microk8s/setup.sh
+++ microk8s/setup.sh
@@ -1,5 +1,4 @@
 sudo apt update && sudo apt upgrade -y
-sudo DEBIAN_FRONTEND=noninteractive apt install -y iptables-persistent 
 sudo apt install uuid -y
 sudo snap install microk8s --classic
 sudo microk8s status --wait-ready
@@ -28,25 +27,18 @@ sudo microk8s kubectl wait --namespace ingress --for=condition=Ready pod -l name
 # --- Verify connectivity on port 80 ---
 echo "[+] Testing HTTP connectivity..."
 if curl -s --max-time 5 http://localhost/ >/dev/null; then
-   echo "[✓] Ingress is serving HTTP traffic at http://$VM_IP/"
+   echo "[✓] Ingress is serving HTTP traffic at port 80"
 fi
 
 # --- Verify connectivity on port 443 (optional, requires TLS configured) ---
 echo "[+] Testing HTTPS connectivity..."
 if curl -sk --max-time 5 https://localhost/ >/dev/null; then
-  echo "[✓] Ingress is serving HTTPS traffic at https://$VM_IP/"
+  echo "[✓] Ingress is serving HTTPS traffic at 443"
 fi
 
-# --- Block external access to kubelet (10250), allow localhost  ---
-echo "[+] Adding firewall rules to restrict kubelet port..." 
-sudo iptables -A INPUT -p tcp --dport 10250 ! -s 127.0.0.1 -j DROP 
-sudo ip6tables -A INPUT -p tcp --dport 10250 ! -s ::1 -j DROP 
-echo "[+] Saving iptables rules for persistence..." 
-sudo netfilter-persistent save
-
-echo "Adding current user to microk8s group..."
+echo "[+] Adding current user to microk8s group..."
 sudo usermod -a -G microk8s $USER && mkdir -p ~/.kube && chmod 0700 ~/.kube
-echo "Setting up kubectl alias..."
+echo "[+] Setting up kubectl alias..."
 echo "alias kubectl='microk8s kubectl'" >> ~/.bash_aliases
 source ~/.bash_aliases
-echo "MicroK8s setup completed. Please log out and log back in for group changes an kubectl alias to take effect."
+echo "[✓] MicroK8s setup completed. Please log out and log back in for group changes an kubectl alias to take effect."
