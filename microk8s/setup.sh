7195d41a6a1203fb20df9300b2bfbade6bcb44e0
cppservergit <cppserver@martincordova.com>
2026-01-17 11:44:52 -0400

    sonarcloud issues fixed

--- microk8s/setup.sh
+++ microk8s/setup.sh
@@ -66,7 +66,7 @@ fi
 echo "[+] Retrieving APIserver2 deployment manifest..."
 curl -s -O -L https://raw.githubusercontent.com/cppservergit/apiserver2/main/microk8s/apiserver2.yaml 
 echo "[+] Deploying APIserver2..."
-microk8s kubectl create namespace cppserver
+sudo microk8s kubectl create namespace cppserver
 sudo microk8s kubectl apply -f apiserver2.yaml
 
 # --- Verify connectivity on port 443 for APIServer2 ---
